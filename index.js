41822ef09b259008f0935d0cb175bd8f6688cb65
incentiv <gustavo.26.guimaraes@gmail.com>
2020-09-17 19:04:42 -0300

    trying firebase

--- index.js
+++ index.js
@@ -80,96 +80,57 @@ function validform() {
 }
 
 function sendData() {
-    // sheets - 1W18Lmkx3-wefHhSm9jzW862YqZhVcVU4zK0odYFKUaQ
-    // API - AIzaSyBluE3w55phlLWICYSZNrsZDn1PJgo3_nw
-    // client ID - 193022482281-pdgrl2i1ru6td457e6ii2emebtkl8jqi.apps.googleusercontent.com
-    // Client secret - wS3l18qYwactgZt9CX3hzdYQ
+    var Nome = document.forms["my-form"]["nome"].value;
+    var IdentificacaoBeneficiario = document.forms["my-form"]["cpfcnpj"].value;
+    var IdentificacaoPessoa = document.forms["my-form"]["cpfcnpj"].value;
+    var DataNascimento = document.forms["my-form"]["birthday"].value;
+    var Email = document.forms["my-form"]["email"].value;
+    var TelefoneCelular = document.forms["my-form"]["phone"].value;
+    var CorporateId = 39;
+    var CodigoContrato = document.forms["my-form"]["plan"].value;
+    var TipoPessoa = document.forms["my-form"]["person_type"].value;
+    var BeneficiarioTitular = document.forms["my-form"]["person_type"].value;
+    var Sexo = document.forms["my-form"]["sex"].value;
+    var StatusBeneficiario = 1;
+    var IdentificacaoCliente = 205;
+    var Produtos = 19;
 
-    gapi.load('client:auth2', this.initClient);
+    var apiObject = {
+        Nome,
+        IdentificacaoBeneficiario,
+        IdentificacaoPessoa,
+        DataNascimento,
+        Email,
+        TelefoneCelular,
+        CorporateId,
+        CodigoContrato,
+        TipoPessoa,
+        BeneficiarioTitular,
+        Sexo,
+        StatusBeneficiario,
+        IdentificacaoCliente,
+        Produtos,
+    }
 
-    const params = {
-        spreadsheetId: '1W18Lmkx3-wefHhSm9jzW862YqZhVcVU4zK0odYFKUaQ',
-        range: 'Sheet1',
-        valueInputOption: 'RAW',
-        inserDataOption: 'INSERT_ROWS',
-    };
-    
-    const valueRangeBody = {
-        'majorDimension': 'ROWS',
-        'values': [submissionValues]
-    };
+    var apiObject = {
+        guti: 'guti',
+        deu: 'sim'
+    }
 
-    let request = gapi.client.sheets.spreadsheets.values.append(params, valueRangeBody);
-    request.then(function (response) {
+    var jsonString = JSON.stringify(apiObject, undefined, 2);
+    var link = document.createElement('a');
+    link.download = 'data.json';
+    var blob = new Blob([jsonString], {type: 'text/plain'});
 
-        console.log(response.result);
-    }, function (reason) {
-        console.error ('error : ' + reason.result.error.message);
-    });
+    // var file = new File([mblob], "filename")
+    var file = new File([blob], Nome + "_" + Date.now(), {type: 'text/plain', lastModified: Date.now()});
+   
+    // Create a root reference
+    var storageRef = firebase.storage().ref();
+    storageRef.put(file).then(function(snapshot) {
+        console.log('Uploaded a blob or file!');
+      });
+      
+
+
 }
-
-// function makeApiCall() {
-//     var params = {
-//       // The ID of the spreadsheet to update.
-//       spreadsheetId: '1W18Lmkx3-wefHhSm9jzW862YqZhVcVU4zK0odYFKUaQ',  // TODO: Update placeholder value.
-
-//       // The A1 notation of a range to search for a logical table of data.
-//       // Values will be appended after the last row of the table.
-//       range: 'Sheet1',  // TODO: Update placeholder value.
-
-//       // How the input data should be interpreted.
-//       valueInputOption: 'RAW',  // TODO: Update placeholder value.
-
-//       // How the input data should be inserted.
-//       insertDataOption: 'INSERT_ROWS',  // TODO: Update placeholder value.
-//     };
-
-//     var valueRangeBody = {
-//         'majorDimension': 'ROWS',
-//         'values': [submissionValues]
-//     };
-
-//     var request = gapi.client.sheets.spreadsheets.values.append(params, valueRangeBody);
-//     request.then(function(response) {
-//       // TODO: Change code below to process the `response` object:
-//       console.log(response.result);
-//     }, function(reason) {
-//       console.error('error: ' + reason.result.error.message);
-//     });
-// }
-
-
-// function initClient() {
-//     var CLIENT_ID = ' 193022482281-pdgrl2i1ru6td457e6ii2emebtkl8jqi.apps.googleusercontent.com';
-//     var API_KEY = 'AIzaSyBluE3w55phlLWICYSZNrsZDn1PJgo3_nw';
-//     var SCOPE = 'https://www.googleapis.com/auth/spreadsheets';
-
-//     gapi.client.init({
-//       'apiKey': API_KEY,
-//       'clientId': CLIENT_ID,
-//       'scope': SCOPE,
-//       'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-//     }).then(function() {
-//       gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
-//       updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-//     });
-// }
-
-// function handleClientLoad() {
-//     gapi.load('client:auth2', initClient);
-// }
-
-// function updateSignInStatus(isSignedIn) {
-//     if (isSignedIn) {
-//     makeApiCall();
-//     }
-// }
-
-// function handleSignInClick(event) {
-//     gapi.auth2.getAuthInstance().signIn();
-// }
-
-// function handleSignOutClick(event) {
-//     gapi.auth2.getAuthInstance().signOut();
-// }
-
